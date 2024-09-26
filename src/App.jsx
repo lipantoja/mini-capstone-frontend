d5827e2bbc673bc35f95878ad4ea6b34f8d1a098
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-09-25 20:32:53 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { PhotosPage } from "./PhotosPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <PhotosPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
